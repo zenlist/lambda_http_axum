f1d6833de86d18943d0df1cbb72367a1e05a2a1a
Bryan Burgers <bryan@burgers.io>
2023-12-28 12:49:28 -0600

    Update to work with axum 0.7

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 use axum::response::IntoResponse;
+use bytes::BytesMut;
+use futures::StreamExt;
 use http::Request;
 use lambda_http::Service;
-use std::{convert::Infallible, error::Error, future::Future, pin::Pin};
+use std::{
+    error::Error,
+    fmt::{Debug, Display},
+    future::Future,
+    pin::Pin,
+};
 
 type LambdaRequest = http::Request<lambda_http::Body>;
 type LambdaResponse = lambda_http::Response<lambda_http::Body>;
@@ -9,11 +16,12 @@ type LambdaError = Box<dyn std::error::Error + Send + Sync>;
 type AxumRequest = http::Request<axum::body::Body>;
 
 #[doc(hidden)]
-pub fn service_fn<S>(s: S) -> Adapter<S>
+pub fn service_fn<R, S, E>(s: S) -> Adapter<S>
 where
-    S: Service<AxumRequest, Error = Infallible>,
-    S::Response: IntoResponse,
-    S::Future: 'static,
+    S: Service<AxumRequest, Response = R, Error = E>,
+    S::Future: Send + 'static,
+    R: IntoResponse,
+    E: Debug + Display,
 {
     Adapter(s)
 }
@@ -21,26 +29,33 @@ where
 #[doc(hidden)]
 pub struct Adapter<S>(S);
 
-pub async fn run<S>(handler: S) -> Result<(), Box<dyn Error + Send + Sync>>
+pub async fn run<R, S, E>(handler: S) -> Result<(), Box<dyn Error + Send + Sync>>
 where
-    S: Service<AxumRequest, Error = Infallible>,
-    S::Response: IntoResponse,
-    S::Future: 'static,
+    S: Service<AxumRequest, Response = R, Error = E>,
+    S::Future: Send + 'static,
+    R: IntoResponse,
+    E: Debug + Display,
 {
     let handler = service_fn(handler);
     lambda_http::run(handler).await
 }
 
-impl<S> Service<LambdaRequest> for Adapter<S>
+impl<R, S, E> Service<LambdaRequest> for Adapter<S>
 where
-    S: Service<AxumRequest, Error = Infallible>,
-    S::Response: IntoResponse,
-    S::Future: 'static,
+    S: Service<AxumRequest, Response = R, Error = E>,
+    S::Future: Send + 'static,
+    R: IntoResponse,
+    E: Debug + Display,
 {
     type Response = LambdaResponse;
     type Error = LambdaError;
-    type Future =
-        Pin<Box<dyn Future<Output = Result<LambdaResponse, Box<dyn Error + Send + Sync>>>>>;
+    type Future = Pin<
+        Box<
+            dyn Future<Output = Result<LambdaResponse, Box<dyn Error + Send + Sync>>>
+                + Send
+                + 'static,
+        >,
+    >;
 
     fn poll_ready(
         &mut self,
@@ -69,8 +84,13 @@ where
             let response = future.await.unwrap();
             let response = response.into_response();
             let (parts, body) = response.into_parts();
-            let body = hyper::body::to_bytes(body).await?;
-            let body = lambda_http::Body::Binary(body.to_vec());
+            let mut response_bytes = BytesMut::default();
+            let mut data_stream = body.into_data_stream();
+            while let Some(result) = data_stream.next().await {
+                let bytes = result?;
+                response_bytes.extend(bytes);
+            }
+            let body = lambda_http::Body::Binary(response_bytes.to_vec());
             Ok(LambdaResponse::from_parts(parts, body))
         };
 
